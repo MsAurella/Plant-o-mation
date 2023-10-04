0f314125df881f2e19a5f73bf63a92b08bd0eb76
¨MsAurella¨ <jongl.s.de@gmail.com>
2023-10-04 15:03:52 +0200

    reading out moisture sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // testing
 #include <Arduino.h>
 
+#define PIN_MOISTURE 36
+#define MOISTURE_MIN 1000 // wet
+#define MOISTURE_MAX 3000 // dry
+
 void setup()
 {
     Serial.begin(9600);
@@ -8,6 +12,8 @@ void setup()
 
 void loop()
 {
+    int raw_moisture = analogRead(PIN_MOISTURE);
     Serial.println("Hello world!");
+    Serial.println(raw_moisture);
     delay(1000);
 }
