a0d6da8aeeeb5e8315280ecce91a7b3303a8bc9f
¨MsAurella¨ <jongl.s.de@gmail.com>
2024-02-10 10:17:50 +0100

    blinky

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "Plant.hpp"
 #include <vector>
 
+// FIREBEETLE
 #define ANALOG_READ_MOISTURE 36
 #define MOISTURE_MIN 1000 // wet
 #define MOISTURE_MAX 3000 // dry
@@ -13,6 +14,9 @@
 #define DIGITAL_WRITE_PUMP 4
 
 #define DIGITAL_IN_BUTTON 27
+
+// ESP WROOM
+#define LED_PIN 2
 
 auto moistureSensor = CapacitiveSoilMoistureSensor(ANALOG_READ_MOISTURE, MOISTURE_MIN, MOISTURE_MAX);
 auto pump = UnderWaterPump(DIGITAL_WRITE_PUMP);
@@ -22,18 +26,29 @@ auto Linsey = Plant("Linsey", pump, moistureSensor);
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    if (button.isSystemSwitchedOn())
-    {
-        Linsey.waterIfRequired();
-    }
-    else
-    {
-        Linsey.shutDown();
-    }
+    // if (button.isSystemSwitchedOn())
+    // {
+    //     Linsey.waterIfRequired();
+    // }
+    // else
+    // {
+    //     Linsey.shutDown();
+    // }
+      // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("Blink");
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);
+   // wait for a second
+  delay(1000);
+
     delay(100);
 }
