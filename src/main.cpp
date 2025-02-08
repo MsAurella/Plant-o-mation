c78258e5bcbb17950b06d47c8c1997d65b251346
¨MsAurella¨ <jongl.s.de@gmail.com>
2025-02-08 18:06:54 +0100

    clangformat

--- src/main.cpp
+++ src/main.cpp
@@ -28,30 +28,30 @@ auto button = Button(DIGITAL_IN_BUTTON);
 
 auto Linsey = Plant("Linsey", pump, moistureSensor);
 
-void setup() {     Serial.begin(115200);
-    // ESPWROOM
-    // pinMode(LED_PIN, OUTPUT); 
-    pinMode(ANALOG_READ_MOISTURE,INPUT);
-    }
+void setup() {
+  Serial.begin(115200);
+  // ESPWROOM
+  // pinMode(LED_PIN, OUTPUT);
+  pinMode(ANALOG_READ_MOISTURE, INPUT);
+}
 
 void loop() {
   if (button.isSystemSwitchedOn()) {
-      Serial.print(">percentage:");
-  Serial.println(moistureSensor.getMoisturePercentage());
-  //   Linsey.waterIfRequired();
-  // } else {
-  //   Linsey.shutDown();
-  pump.off();
+    Serial.print(">percentage:");
+    Serial.println(moistureSensor.getMoisturePercentage());
+    //   Linsey.waterIfRequired();
+    // } else {
+    //   Linsey.shutDown();
+    pump.off();
   } else {
     pump.on();
     Serial.println("off");
   }
   // delay(100);
-      // turn the LED on (HIGH is the voltage level)
+  // turn the LED on (HIGH is the voltage level)
   // Serial.println("Blink");
   // wait for a second
   // turn the LED off by making the voltage LOW
-   // wait for a second
+  // wait for a second
   delay(100);
-
 }
